aacac6b45daafdd145729363a440cb086770b57a
Stef Claus <stef.claus@gmail.com>
2018-07-24 02:07:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string)
-  console.log(`${string})
-  return string.toUpperCase()
+function logShout(string) {
+  console.log(`${string}
   
