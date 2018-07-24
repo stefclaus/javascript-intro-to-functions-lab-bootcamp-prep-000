ba66ab65d32bfb9bc5b8e5187ec2a0d668588e57
Stef Claus <stef.claus@gmail.com>
2018-07-24 03:11:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
    }  else if (`${string}`.toUpperCase() === mixedCase) {
       return "YES INDEED!"
       
-   }  else if (`${string}`.toUpperCase() === mixedCase) {
-      return "YES INDEED!"
+   }  else if (`${string}` === "I love you, Grandma") {
+      return "I love you, too"
   }
 }
