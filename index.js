5c6c820af6c7443fe13bb1e04b6628a5ab2edc22
Stef Claus <stef.claus@gmail.com>
2018-07-24 02:42:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logWhisper(string) {
    console.log(`${string}`.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toUpperCase() === uppercase) {
+  if (string.toLowerCase() === uppercase) {
       return "I can't hear you!"
 }
