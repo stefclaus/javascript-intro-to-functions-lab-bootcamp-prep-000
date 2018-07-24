b36d97d17ed8efbef47079573746435e60495546
Stef Claus <stef.claus@gmail.com>
2018-07-24 03:08:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var mixedCase = string
-  
+  var 
   if (`${string}`.toLowerCase() === mixedCase) {
       return "I can't hear you!"
       
