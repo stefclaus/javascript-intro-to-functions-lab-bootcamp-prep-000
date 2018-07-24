320aad3f029c33c47c624445d0ba45a8249d5b5e
Stef Claus <stef.claus@gmail.com>
2018-07-24 03:10:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ function logShout(string) {
 function logWhisper(string) {
    console.log(`${string}`.toLowerCase())
 }
-
-
 function sayHiToGrandma(string) {
-  var mixedCase = "I love you, Grandma"
+  var mixedCase = string
   if (`${string}`.toLowerCase() === mixedCase) {
       return "I can't hear you!"
       
    }  else if (`${string}`.toUpperCase() === mixedCase) {
       return "YES INDEED!"
-      } 
+      
+  }   else if (`${string}`) === "I love you, Grandma." {
+      return "I love you, too
   }
+}
